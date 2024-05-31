fcb8ba6cfb96a4ba90b103ac488db62c50d72ee1
manoj <manojpoojary2004@gmail.com>
2024-05-31 23:48:34 +0530

    Adding shopdetails

--- Admin Panel/Shopdetails.js
+++ Admin Panel/Shopdetails.js
@@ -5,14 +5,91 @@ let products_action = document.querySelector('.products_action');
 let product_add_form = document.querySelector(".add_product_form");
 
 function shopdetails_click_handle() {
-    shop_details_feilds.style.display = "block";
-    products_action.style.display = "none";
+    // Check shop is their or not 
+    if (localStorage.getItem('shop') == '1') {
+        // shop_details_retrive_to_the_localStorage
+        let Shop_details_container = JSON.parse(localStorage.getItem('shopdetails'));
+        let shop_view_container = document.querySelector('.shop_view_conatiner');
+        shop_view_container.style.display = "block";
+        let shop_img_container = document.createElement('img');
+        shop_img_container.className="shop_img_container";
+        shop_img_container.src = Shop_details_container.ShopImgurl;
+        shop_view_container.appendChild(shop_img_container);
+        
+        // Shop name display
+        let shop_name_container=document.createElement('h3');
+        shop_name_container.innerHTML=Shop_details_container.shopname;
+        shop_name_container.className="shop_name_container";
+        shop_view_container.appendChild(shop_name_container);
+        console.log(shop_view_container);
+    } else {
+        shop_details_feilds.style.display = "block";
+        products_action.style.display = "none";
+
+    }
 
 
 }
+
 function productdetails_click_handle() {
     product_display_root_container.style.display = "block"
     products_action.style.display = "block";
     shop_details_feilds.style.display = "none";
     product_add_form.style.display = "none";
 }
+
+
+
+
+let shop_details_form = document.querySelector('#SD_Form');
+shop_details_form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    // Formdata object used to get the input data and access the data based on the 'name' attribute .
+    const form_data = new FormData(e.target);
+    const entry = Object.fromEntries(form_data.entries());
+    // if (((entry.Start_time && entry.End_time>= 1) && (entry.Start_time && entry.End_time <=12)) && ((entry.Start_minutes && entry.End_minutes>= 1) && (entry.Start_minutes && entry.End_minutes <=60))) {
+    // let Start_Time=entry.Start_time+" : "+entry.Start_minutes+"  "+entry.Start_AMPM;
+    // let End_Time=entry.End_time+" : "+entry.End_minutes+"  "+entry.End_AMPM;
+    // console.log(Start_Time+" - "+End_Time);
+
+    // }else{
+    //     alert("Invalid Time");
+
+    // }
+    let Start_Time = entry.Start_time + " : " + entry.Start_minutes + "  " + entry.Start_AMPM;
+    let End_Time = entry.End_time + " : " + entry.End_minutes + "  " + entry.End_AMPM;
+
+
+    // Shop id access to the localstorage
+    let admin_id = localStorage.getItem('admin_id');
+    let shop_details_form_data = {
+        shop_id: admin_id,
+        shop_name: entry.sname,
+        shop_owner_name: entry.Oname,
+        shop_category: entry.Category,
+        shop_location: entry.Location,
+        shop_timings: Start_Time + " - " + End_Time,
+        shop_imgUrl: entry.ImgUrl,
+        shop_logoUrl: entry.logoUrl,
+        shop_contact_details: entry.contactdetails,
+        shop_email: entry.email,
+    }
+    let response = await (await fetch('http://localhost/BACKEND/ADMIN/ADMIN_ACTION/shopdetails.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(shop_details_form_data)
+    })
+    ).json();
+    if (response) {
+
+        localStorage.setItem('shop', '1');
+        if (localStorage.getItem('shop') == '1') {
+            window.location.reload();
+            localStorage.removeItem('shop');
+        }
+    }
+
+    console.log(response);
+})
--- Admin Panel/dashboard.js
+++ Admin Panel/dashboard.js
@@ -9,10 +9,11 @@ function logout() {
     let logout_conformation = window.confirm('Are you sure you want to log out?');
     if (logout_conformation) {
         window.location.href = "http://127.0.0.1:5500/Admin%20Panel/dashboard.html?logout=1";
-
+        localStorage.clear();
     }
 
 }
+// logout Check
 if (logout_status == 1) {
     window.location.href = "http://127.0.0.1:5500/Account%20Setup%20Process/AdminLogin.html";
 } else {
@@ -21,13 +22,17 @@ if (logout_status == 1) {
     const query = new URLSearchParams(urlObj.search);
     const query_admin_id = query.get('admin_id');
 
+    // Store id to the localstorage
+    localStorage.setItem('admin_id',query_admin_id);
+    // Access localstorage data
+    let localstorage_admin_id=localStorage.getItem('admin_id');
 
     let shop_owner_name_container = document.querySelector(".shop_owner_name");
     let shop_profile_img_container = document.querySelector(".profile_img");
 
     window.addEventListener('load', async () => {
         const admin_id_container = {
-            admin_id: query_admin_id
+            admin_id: localstorage_admin_id
         };
 
         let dashboard_backend_response = await (await fetch('http://localhost/BACKEND/Admin/dashboard.php', {
@@ -39,18 +44,20 @@ if (logout_status == 1) {
         })
         ).json();
 
-        console.log("backend sended data", dashboard_backend_response);
+        // console.log("backend sended data", dashboard_backend_response);
         if (dashboard_backend_response.msg) {
             shop_owner_name_container.innerHTML = "User";
+            console.log(dashboard_backend_response);
 
         } else {
+            // Storing Shop details to the localstorage
+            localStorage.setItem('shopdetails',JSON.stringify(dashboard_backend_response));
             shop_owner_name_container.innerHTML = dashboard_backend_response.ownername;
             shop_profile_img_container.src = dashboard_backend_response.logoUrl;
         }
+        console.log("dashboard",dashboard_backend_response);
     });
-
 }
-
 
 
 
